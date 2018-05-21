ecd15baf0b4620bcd6b5b4d6633e38a09578a028
Joseph Balsamo <joseph.balsamo@gmail.com>
2018-05-21 18:31:46 +0000

    Fixed conf file

--- conf/variables.sh
+++ conf/variables.sh
@@ -4,12 +4,14 @@
 DEFAULT_OBJ=20
 DEFAULT_MPP=0.5
 CANCER_TYPE=quip
-MONGODB_HOST=quip3.bmi.stonybrook.edu
+MONGODB_HOST=quip-data
 MONGODB_PORT=27017
+FINDAPI_HOST=quip-findapi
+FINDAPI_PORT=3000
 HEATMAP_VERSION=lym_v1
 
 # Base directory
-BASE_DIR=/data01/shared/lehhou/u24_lymphocyte/
+BASE_DIR=/root/src/lymph_pipeline/
 
 # The username you want to download heatmaps from
 USERNAME=john.vanarnam@gmail.com
@@ -45,21 +47,21 @@ NEC_CNN_TRAINING_DEVICE=gpu1
 NEC_CNN_PRED_DEVICE=gpu1
 
 # Load modules
-module purge
-module load matlab
-module load mongodb/3.2.0
-module load jdk8/1.8.0_11
-module load openslide/3.4.0
-module load extlibs/1.0.0
-module load ITK/4.6.1
-module load cuda75
-module load anaconda2/4.4.0
-module load imagemagick/7.0.7
-export PATH=/home/lehhou/git/bin/:${PATH}
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/cm/shared/apps/anaconda2/current/lib/"
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/cm/shared/apps/cuda75/toolkit/7.5.18/lib64/"
-export CUDA_HOME=/cm/shared/apps/cuda75
-export LIBTIFF_CFLAGS="-I/cm/shared/apps/extlibs/include" 
-export LIBTIFF_LIBS="-L/cm/shared/apps/extlibs/lib -ltiff" 
-source ~/theano/bin/activate
+###module purge
+###module load matlab
+###module load mongodb/3.2.0
+###module load jdk8/1.8.0_11
+###module load openslide/3.4.0
+###module load extlibs/1.0.0
+###module load ITK/4.6.1
+###module load cuda75
+###module load anaconda2/4.4.0
+###module load imagemagick/7.0.7
+###export PATH=/home/lehhou/git/bin/:${PATH}
+###export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/cm/shared/apps/anaconda2/current/lib/"
+###export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/cm/shared/apps/cuda75/toolkit/7.5.18/lib64/"
+###export CUDA_HOME=/cm/shared/apps/cuda75
+###export LIBTIFF_CFLAGS="-I/cm/shared/apps/extlibs/include" 
+###export LIBTIFF_LIBS="-L/cm/shared/apps/extlibs/lib -ltiff" 
+###source ~/theano/bin/activate
 
